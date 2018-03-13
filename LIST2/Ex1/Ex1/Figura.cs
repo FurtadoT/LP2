68663087fe31c86b872de3360a8532cd86eddc18
Tobias <tobias_fs_@hotmail.com>
2018-03-13 20:59:36 -0300

    Modified exercise4 to match exercise5 requisites

---
+++ LIST2/Ex1/Ex1/Figura.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_04_Ex_01
+{
+    abstract class Figura
+    {
+        private int x, y;
+
+        public Figura(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        //O modificador 'virtual' indica que o método pode ser
+        //sobrescrito por subclasses desta classe.
+        //SÓ QUE NÃO TEM MAIS VIRTUAL NE AMIGUINHO
+        abstract public double Area();
+
+        public int X
+        {
+            get
+            {
+                return x;
+            }
+
+            set
+            {
+                x = value;
+            }
+        }
+
+        public int Y
+        {
+            get
+            {
+                return y;
+            }
+
+            set
+            {
+                y = value;
+            }
+        }
+    }
+}
---
+++ LIST2/Ex1/Ex1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_04_Ex_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Figura> figuras = new List<Figura>();
+            double areaTot = 0;
+
+            //Instancia as figuras
+            for (int contador = 0; contador < 50; contador++)
+            {
+                figuras.Add(new Circulo(contador, contador, contador));
+                figuras.Add(new Retangulo(contador, contador, contador, contador));
+                figuras.Add(new TrianguloRetangulo(contador, contador, contador, contador));
+            }
+
+            foreach (Figura figura in figuras)
+            {
+                areaTot += figura.Area();
+            }
+
+            Console.WriteLine("Area total das figuras: " + areaTot);
+
+            Console.Read();
+        }
+    }
+}
